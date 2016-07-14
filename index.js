e7e54615e934e6b7b5e0a3603523be1a899dab66
Daniel C <bitclaw@gmail.com>
2016-07-14 11:49:24 -0500

    Add options for cli.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 #!/usr/bin/env node
 
+'use strict';
+
+const program = require('commander');
+
+program
+    .version('1.0.0')
+    .option('-g, --generate-array','Generates big array for Codility exercises')
+    //.option('-v, --verbose [optional]','Optional option to increase verbosity')
+    //.option('-I, --another-input <required>','required user input')
+    .parse(process.argv); // end with parse to parse through the input
+
